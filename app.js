9919b39ba3a5e062737b91f4affb1b2a0fe28d90
imbriankeeley <imbriankeeley@gmail.com>
2023-10-08 17:34:59 -0400

    Added operator function and variabes

--- app.js
+++ app.js
@@ -13,3 +13,11 @@ function multiply(a, b){
 function divide(a, b){
     return a / b;
 };
+
+let firstNumber = 0;
+let operator = 0;
+let secondNumber = 0;
+
+function operate(firstNumber, operator, secondNumber){
+    
+}
