38952e78a6a510a270c1455c24dd8227ae2a9f9c
imbriankeeley <bk3700@proton.me>
2023-11-17 10:07:20 -0500

    Added some calculator logic

--- app.js
+++ app.js
@@ -21,3 +21,27 @@ let secondNumber = 0;
 function operate(firstNumber, operator, secondNumber){
     operator(firstNumber, secondNumber);
 }
+
+let displayValue
+
+const display = document.getElementById('numbers')
+
+const numberSeven = document.getElementById("seven");
+numberSeven.addEventListener('click', function() {
+    display.textContent = `${displayValue}` + '7'
+    return displayValue;
+});
+
+const numberEight = document.getElementById("eight");
+numberEight.addEventListener('click', function() {
+    displayValue = 8;
+    display.textContent = `${displayValue}`
+    return displayValue;
+});
+
+const numberNine = document.getElementById("nine");
+numberNine.addEventListener('click', function() {
+    displayValue = 9;
+    display.textContent = `${displayValue}` + '9'
+    return displayValue;
+});
