13f97c40357cb8646aa0cd4ea6ef63bdb5e95e15
imbriankeeley <bk3700@proton.me>
2023-12-10 19:20:03 -0500

    Restarted logic

--- app.js
+++ app.js
@@ -1,47 +1,7 @@
-function add(a, b){
-    return a + b;
-};
+document.addEventListener("DOMContentLoaded", function(){
 
-function subtract(a, b){
-    return a - b;
-};
+})
 
-function multiply(a, b){
-    return a * b;
-};
 
-function divide(a, b){
-    return a / b;
-};
 
-let firstNumber = 0;
-let operator = 0;
-let secondNumber = 0;
 
-function operate(firstNumber, operator, secondNumber){
-    operator(firstNumber, secondNumber);
-}
-
-let displayValue
-
-const display = document.getElementById('numbers')
-
-const numberSeven = document.getElementById("seven");
-numberSeven.addEventListener('click', function() {
-    display.textContent = `${displayValue}` + '7'
-    return displayValue;
-});
-
-const numberEight = document.getElementById("eight");
-numberEight.addEventListener('click', function() {
-    displayValue = 8;
-    display.textContent = `${displayValue}`
-    return displayValue;
-});
-
-const numberNine = document.getElementById("nine");
-numberNine.addEventListener('click', function() {
-    displayValue = 9;
-    display.textContent = `${displayValue}` + '9'
-    return displayValue;
-});
